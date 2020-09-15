900aba1f63ea606af2ab26e4313b52077aed168a
Satoshi Tanaka <satoshi.tanaka@crowdworks.co.jp>
2020-09-15 21:26:53 +0900

    ダウンロード対象を追加

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ if __name__ == '__main__':
     sauna_iko = Audee(url='https://park.gsj.mobi/program/voice/100000061',
                       album="清水みさとの、サウナいこ？")
     sauna_iko.download()
+
+    sauna_iko = Audee(url='https://audee.jp/program/voice/40889',
+                      album="伊藤沙莉のsaireek channel")
+    sauna_iko.download()
